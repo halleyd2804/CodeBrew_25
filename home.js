14950dfb0a99b033f3c8f4d3378fd83a8dafd95f
Shannon Yap <kxfiiclub@gmail.com>
2025-04-25 15:25:29 +1000

    Added dropdown list and continue button

---
+++ home.js
@@ -0,0 +1,8 @@
+function toggleDropdown() {
+    document.getElementById("dropdownMenu").classList.toggle("show");
+}
+
+function selectSoil(soil) {
+    open('https://www.youtube.com/watch?v=qh6JUB1jOvw&ab_channel=%E4%B9%85%E6%88%91%E3%83%AC%E3%82%AA%2FKugaLeo')
+    // Placeholder link
+}
